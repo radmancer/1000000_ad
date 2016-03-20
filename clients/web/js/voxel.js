c48871bc24cc1d2b7500f1fcf39da89e956eda66
whetstone11 <mcdowellj87@gmail.com>
2016-03-20 16:20:13 -0400

    Reverting sessionStorage changes.

--- clients/web/js/voxel.js
+++ clients/web/js/voxel.js
@@ -15,8 +15,7 @@ function updateGridOnMouseUp(){
 //In order for the stage to be reset, a page redirect is necessary and all point 
 //data must be placed in the query string to be read by the page upon refresh.
 function resetStage(){
-    /*sessionStorage.setItem("mesh", document.getElementById("importExport").value);*/
-    sessionStorage.mesh = document.getElementById("importExport").value;
+    sessionStorage.setItem("mesh", document.getElementById("importExport").value);
     window.location = window.location;
 /*
 	var voxelCoordinates = "{";
