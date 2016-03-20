de2a77b113c549144cf6aeea3604d0bd2f41289f
whetstone11 <mcdowellj87@gmail.com>
2016-03-19 22:48:41 -0400

    changed rad to the way it is in interface_unity branch.

--- clients/web/js/voxel.js
+++ clients/web/js/voxel.js
@@ -190,13 +190,11 @@ function updateGrid(direction){
 			globalCursorZ -= voxelDimension;
 			cursorZ = globalCursorZ + "px";
 			cursorZ = "translateZ(" + cursorZ + ")";
-alert('in');
 			break;
 		case 'out':
 			globalCursorZ += voxelDimension;
 			cursorZ = globalCursorZ + "px";
 			cursorZ = "translateZ(" + cursorZ + ")";
-alert('out');
 			break;
 	}
 
